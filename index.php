b55eda9651a90dc6482c12550d737f51ee3999aa
antonkio <121202809+antonkio@users.noreply.github.com>
2022-12-22 02:59:32 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en-US" >
+	<head>
+		<meta charset="UTF-8">
+		<title>Anton Kio Designer and Artist</title>
+		<meta name="description" content="Anton Kio Designer Artist - Anton Kio, Graphic Designer and Artist" />
+		<meta name="keywords" content="Maintenance Mode" />
+		<meta name="robots" content="index, follow" />
+		<link rel="stylesheet" id="frontend-css" href="/style.min.css" media="all">
+<style type="text/css">
+.wrap h1 { color: #ffffff; }
+.wrap h2 { color: #ffffff; }
+.wrap .footer_links a, .wrap .author_link a { color: #ffffff; }
+.background { background: url("https://designkio.com/wp-content/uploads/2022/06/designkio_fon_03.jpg") no-repeat center top fixed; background-size: cover; }
+</style>
+	</head>
+	<body class="background">
+		
+		<div class="wrap" role="main">
+							<!-- Heading -->
+				<h1>Maintenance mode</h1>
+			
+							<!-- Text -->
+				<h2><p style="font-size: 0.5em;line-height: 1.5">Sorry for the inconvenience.<br />Our website is currently undergoing scheduled maintenance.<br />Thank you for your understanding.</p>
+<p><strong>Anton Kio </strong><em><strong>Designer Artist</strong></em></p>
+<p><a href="https://www.behance.net/antonkio" target="_blank" rel="noopener">Behance</a><br /><a href="https://www.instagram.com/designkiocom/" target="_blank" rel="noopener">Instagram</a><br /><a href="https://www.fl.ru/users/tonitoon/opinions/#/" target="_blank" rel="noopener">fl.ru</a><br /><a href="https://vk.com/kioanton" target="_blank" rel="noopener">vk.com</a><br /><a href="https://t.me/antonkio" target="_blank" rel="noopener">Telegram</a><br /><a href="https://wa.me/+380509159864" target="_blank" rel="noopener">WhatsApp</a><br /><br /></p></h2>
+				
+			
+			
+			
+			
+			
+					</div>
+	</body>
+</html>
+	
